b77bdd93a948ece02012063fd352a879fef505d2
Jolienai <jolienai@Jolienais-MBP-2.Home>
2022-07-20 15:07:46 +0100

    added docker-compose-local.yml

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,10 +6,9 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/jolienai/go-products-api/cmd/app/controllers"
+	"github.com/jolienai/go-products-api/cmd/app/jobs"
 
 	"github.com/jolienai/go-products-api/cmd/app/database"
-
-	"github.com/jolienai/go-products-api/cmd/app/jobs"
 
 	"gorm.io/gorm"
 
@@ -27,19 +26,16 @@ func main() {
 
 	logger.Info("Starting products-api...")
 
-	connectionString := os.Getenv("POSTGRES_CONNECTION_STRING")
-	if connectionString == "" {
+	dsn := os.Getenv("POSTGRES_CONNECTION_STRING")
+	if dsn == "" {
 		panic("POSTGRES_CONNECTION_STRING environment variable must be set")
 	}
-
-	//db, err := gorm.Open("postgres", connectionString)
-	dsn := os.Getenv("POSTGRES_CONNECTION_STRING")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
 	}
 
-	logger.Info("Trying to apply the migrations..." + connectionString)
+	logger.Info("Trying to apply the migrations..." + dsn)
 	repository := database.NewProductsRepository(db)
 	repository.AutoMigrate()
 	logger.Info("Migrations applied...")
